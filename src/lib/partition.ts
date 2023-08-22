08a7c893251aabcfb0d60eb0e591140a36309e9e
Daniel Silva <daniel.silva.prg@gmail.com>
2023-08-22 12:43:36 +0100

    partition function

---
+++ src/lib/partition.ts
@@ -0,0 +1,8 @@
+export const partition = <T>(arr: T[], fn: (currentValue: T) => boolean) =>
+	arr.reduce(
+		(reducedArr: [T[], T[]], currentValue: T) => {
+			reducedArr[fn(currentValue) ? 0 : 1].push(currentValue);
+			return reducedArr;
+		},
+		[[],[]]
+	);
